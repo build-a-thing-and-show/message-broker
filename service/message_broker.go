544722e91919fd522219242ca103ae3e54d5381b
Reza <rzgholizadeh@gmail.com>
2025-01-14 20:24:44 -0800

    Remove broker

--- service/message_broker.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "context"
-
-type MessageBroker interface {
-	Publish(ctx context.Context, msg *Message) error
-	Subscribe(ctx context.Context, topic string) (<-chan *Message, error)
-}
